08492e307f338344615f14bdb8e17e7cea7490cf
Severikerkka <severi.kerkka@edu.salpaus.fi>
2020-11-23 08:59:40 +0000

    20201123

--- scriptemail.js
+++ scriptemail.js
@@ -9,4 +9,21 @@ function sendJSON(){
   let xhr = new XMLHttpRequest();
   let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
 
+xhr.open("POST", url, true);
+
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function(){
+  if(xhr.readyState === 4 && xhr.status === 200){
+    console.loh("yhteys toimii")
+   }
+  }
+};
+var data = JSON.stringify({
+  "EmailMsg": "Tähän tulee postin sisältö", //kirjoittaa sähköpostin sisällön
+  "EmailAddress": "severi.kerkka@edu.salpaus.fi", //viestin kirjoittajan sähköposti
+  "EmailTo": "seve9191@gmail.com", //oma sähköposti
+  "EmailName": "Teppo Tyyppi" //nimi kentän sisältö
+});
+xhr.send(data);
 }
